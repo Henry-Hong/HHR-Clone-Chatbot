a32e1724851dccf69b255d2a0bae3a0a31518c11
Henry-Hong <hhr8540@naver.com>
2024-04-21 22:23:45 +0900

    feat : link 버튼 추가

--- src/components/customs/Main/Chat/ImageResponseCard.tsx
+++ src/components/customs/Main/Chat/ImageResponseCard.tsx
@@ -2,6 +2,8 @@ import Flex from '@/components/cores/Flex';
 import { TypeImageResponseCard, TypeImageResponseCardButton } from './types';
 import { twMerge } from 'tailwind-merge';
 import { useAppContext } from '@/context';
+import _ from 'lodash';
+import Svg from '@/components/cores/Svg';
 
 export default function ImageResponseCard({ msg, isLast }: { msg: TypeImageResponseCard; isLast: boolean }) {
   const buttons = msg.buttons;
@@ -15,6 +17,40 @@ export default function ImageResponseCard({ msg, isLast }: { msg: TypeImageRespo
 }
 
 function OptionButton({ button, isLast }: { button: TypeImageResponseCardButton; isLast: boolean }) {
+  const isLink = _.first(button.text) === '@';
+  const Component = isLink ? OptionButton.Link : OptionButton.Btn;
+  return <Component button={button} isLast={isLast} />;
+}
+
+OptionButton.Link = function Link({ button }: { button: TypeImageResponseCardButton }) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, ...label] = button.text;
+  const link = button.value;
+
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className={twMerge(
+        'rounded-full p-1 px-3 border-[2px] text-gray-500 border-blue-500 transition-all hover:bg-gray-100 shrink-0 shadow-md'
+      )}
+    >
+      <Flex className="gap-1 font-bold">
+        <Svg
+          iconName="ic_link"
+          svgProps={{
+            width: '20px',
+            height: '20px',
+            strokeWidth: '2px',
+          }}
+        />
+        <p>{label}</p>
+      </Flex>
+    </a>
+  );
+};
+
+OptionButton.Btn = function Btn({ button, isLast }: { button: TypeImageResponseCardButton; isLast: boolean }) {
   const { clickedBtns, addClickedBtn } = useAppContext();
   const isClicked = clickedBtns.some((btn) => btn === button.value);
 
@@ -36,4 +72,4 @@ function OptionButton({ button, isLast }: { button: TypeImageResponseCardButton;
       {button.text}
     </button>
   );
-}
+};
--- src/components/customs/Main/Chat/PlainText.tsx
+++ src/components/customs/Main/Chat/PlainText.tsx
@@ -4,12 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export default function PlainText({ msg, isLast }: { msg: string; isLast: boolean }) {
   return (
     <Flex
+      variants="verticalLeft"
+      dangerouslySetInnerHTML={{ __html: msg }}
       className={twMerge(
         'text-left break-words border-[1.5px] rounded rounded-r-2xl rounded-bl-2xl bg-gray-100 py-2 px-3 box-content transition-all',
         isLast ? 'border-gray-400/50' : 'border-transparent'
       )}
-    >
-      {msg}
-    </Flex>
+    ></Flex>
   );
 }
