40391e244fcab43e6ff3879438727923fcdba62a
Henry-Hong <hhr8540@naver.com>
2024-06-16 00:49:07 +0900

    fix : FAQ 버튼 경우 clicked를 해당 버튼에만 적용

--- src/components/customs/Main/Chat/FallbackIntent.tsx
+++ src/components/customs/Main/Chat/FallbackIntent.tsx
@@ -3,6 +3,7 @@ import ImageResponseCard from './ImageResponseCard';
 import PlainText from './PlainText';
 
 export default function FallbackIntent({ isLast }: { isLast: boolean }) {
+  const uuidRef = React.useRef<string>(new Date().getTime().toString());
   return (
     <React.Fragment>
       <PlainText msg={'다른 질문이 있으신가요?'} isLast={false} />
@@ -12,7 +13,7 @@ export default function FallbackIntent({ isLast }: { isLast: boolean }) {
           buttons: [
             {
               text: '자주 묻는 질문 보기',
-              value: 'FAQ',
+              value: uuidRef.current,
             },
           ],
         }}
