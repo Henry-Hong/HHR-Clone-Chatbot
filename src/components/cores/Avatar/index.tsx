df255ee15e3fa2d639752e0b30dcecf0b0e212d5
Henry-Hong <hhr8540@naver.com>
2024-06-23 14:08:44 +0900

    layout : changed header, avatar img

--- src/components/cores/Avatar/index.tsx
+++ src/components/cores/Avatar/index.tsx
@@ -1,10 +1,10 @@
-import DefaultImg from '/img_space.jpeg';
+import DefaultImg from '/img_hhr.jpeg';
 
 export default function Avatar({ src, alt }: { src?: string; alt?: string }) {
   const _src = src || DefaultImg;
   const _alt = alt || 'avatar';
   return (
-    <div className="w-[42px] flex-shrink-0 aspect-square rounded-full border-2 overflow-hidden hover:rotate-180 transition-all ">
+    <div className="w-[42px] flex-shrink-0 aspect-square rounded-full border-2 overflow-hidden ">
       <img src={_src} alt={_alt} className="w-full h-full" />
     </div>
   );
